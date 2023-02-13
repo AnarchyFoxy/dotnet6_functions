9659a0f0d26f5a04c40446bbc3e796d86fffc1aa
Astryda Malinowska <astamalinowska@outlook.com>
2023-02-13 20:40:34 +0100

    working with protocols

--- Protokoly/Program.cs
+++ Protokoly/Program.cs
@@ -1,1 +1,13 @@
 ﻿using static System.Console;
+using System.Diagnostics;
+
+
+
+Trace.Listeners.Add(new TextWriterTraceListener(
+    File.CreateText(Path.Combine(Environment.GetFolderPath(
+        Environment.SpecialFolder.DesktopDirectory), "log.txt"))));
+
+Trace.AutoFlush = true;
+
+Debug.WriteLine("Typ Debug mówi, że już jest gotowy.");
+Trace.WriteLine("Typ Trace mówi, że już jest gotowy.");Working with protocols
