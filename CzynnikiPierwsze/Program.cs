500f3b4643420a72d6d8943037737477f8047d7f
Astryda Malinowska <astamalinowska@outlook.com>
2023-02-14 22:40:40 +0100

    Finished with tests

--- CzynnikiPierwsze/Program.cs
+++ CzynnikiPierwsze/Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BibliotekaCzynnikiPierwsze;
+using static System.Console;
 
+Write("Wpisz liczbę między 1 i 1000: ");
+
+if (int.TryParse(ReadLine(), out int number))
+{
+    WriteLine(format: "Czynniki pierwsze z {0} to {1}",
+        arg0: number,
+        arg1: Czynniki_pierwsze.Czynniki_Pierwsze(number));
+}
--- CzynnikiPierwszeTesty/CzynnikiPierwszeTesty.cs
+++ CzynnikiPierwszeTesty/CzynnikiPierwszeTesty.cs
@@ -1,10 +1,80 @@
-﻿namespace CzynnikiPierwszeTesty;
+﻿using Xunit;
+using BibliotekaCzynnikiPierwsze;
 
-public class UnitTest1
+namespace CzynnikiPierwszeTesty;
+
+public class Czynniki_pierwsze_testy
 {
     [Fact]
-    public void Test1()
+    public void Czynniki_pierwsze_z_40()
     {
+        //arrange
 
+        int number = 40;
+        string expected = "5 x 2 x 2 x 2";
+
+        //act
+        string actual = Czynniki_pierwsze.Czynniki_Pierwsze(number);
+
+        //assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Czynniki_pierwsze_z_99()
+    {
+        // arrange
+        int number = 99;
+        string expected = "11 x 3 x 3";
+
+        // act
+
+        string actual = Czynniki_pierwsze.Czynniki_Pierwsze(number);
+
+        //assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Czynniki_pierwsze_z_519()
+    {
+        // arrange
+        int number = 519;
+        string expected = "173 x 3";
+
+        // act
+
+        string actual = Czynniki_pierwsze.Czynniki_Pierwsze(number);
+
+        //assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Czynniki_pierwsze_z_997()
+    {
+        // arrange
+        int number = 997;
+        string expected = "997";
+
+        // act
+        string actual = Czynniki_pierwsze.Czynniki_Pierwsze(number);
+
+        //assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Czynniki_pierwsze_z_1000()
+    {
+        // arrange
+        int number = 1000;
+        string expected = "5 x 5 x 5 x 2 x 2 x 2";
+
+        // act
+        string actual = Czynniki_pierwsze.Czynniki_Pierwsze(number);
+
+        // assert
+        Assert.Equal(expected, actual);
     }
 }
