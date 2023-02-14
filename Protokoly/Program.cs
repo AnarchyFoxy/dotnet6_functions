bec0740509d118c83ce72dff93a707c1b86c1138
Astryda Malinowska <astamalinowska@outlook.com>
2023-02-14 19:54:48 +0100

    Finished with protocols

--- Protokoly/Program.cs
+++ Protokoly/Program.cs
@@ -1,5 +1,6 @@
 ﻿using static System.Console;
 using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
 
 
 
@@ -11,3 +12,21 @@ Trace.AutoFlush = true;
 
 Debug.WriteLine("Typ Debug mówi, że już jest gotowy.");
 Trace.WriteLine("Typ Trace mówi, że już jest gotowy.");
+
+ConfigurationBuilder tworca_konfiguracji = new();
+
+tworca_konfiguracji.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("Appsettings.json",
+    optional: true, reloadOnChange: true);
+
+IConfigurationRoot konfiguracja = tworca_konfiguracji.Build();
+
+TraceSwitch ts = new(
+    displayName: "Przelacznik",
+    description: "Ten przelacznik jest ustawiany przez konfigurację z pliku JSON.");
+
+konfiguracja.GetSection("Przelacznik").Bind(ts);
+
+Trace.WriteLineIf(ts.TraceError, "Poziom błędów");
+Trace.WriteLineIf(ts.TraceWarning, "Poziom ostrzeżeń");
+Trace.WriteLineIf(ts.TraceInfo, "Poziom informacji");
+Trace.WriteLineIf(ts.TraceVerbose, "Poziom ogólny");
