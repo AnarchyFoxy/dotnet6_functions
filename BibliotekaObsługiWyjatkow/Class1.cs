0aec108bcb04698ef74459a825bbddb255f282ea
Astryda Malinowska <astamalinowska@outlook.com>
2023-02-14 21:29:19 +0100

    added content to exception library, created new app for exception handling

--- BibliotekaObsługiWyjatkow/Class1.cs
+++ BibliotekaObsługiWyjatkow/Class1.cs
@@ -1,6 +1,18 @@
-﻿namespace BibliotekaObsługiWyjatkow;
-public class Class1
-{
+﻿using static System.Console;
+namespace CS10;
 
+public class Kalkulator
+{
+    public static void Ghama()
+    {
+        WriteLine("W metodzie ghama");
+        Delta();
+    }
+
+    private static void Delta()
+    {
+        WriteLine("W metodzie Delta");
+        File.OpenText("Nieprawidłowa ścieżka do pliku");
+    }
 }
 
